8181c5ffa69e788a868defd5bbb8a8d55c4b8cd7
MYusufSARI <137108609+MYusufSARI@users.noreply.github.com>
2023-07-30 16:09:02 +0300

    Added details

---
+++ Assets/Scripts/ObjectsController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ObjectsController : MonoBehaviour
+{
+    [SerializeField] private GameObject Sun;
+    [SerializeField] private float speed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+        
+    }
+}
